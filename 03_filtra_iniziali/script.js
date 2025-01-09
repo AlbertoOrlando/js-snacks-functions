bb161ed80d0e90954de1c9fa871b43a771fb9a3b
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-09 18:05:47 +0100

    feat esercizo parole che iniziano con la stessa vocale

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtraIniziali(nuovoArray, lettera){
+    let iniziali = [];
+    for (let i = 0; i < nuovoArray.length; i++){
+        if (nuovoArray[i][0] === lettera){
+            iniziali.push(nuovoArray[i]);
+        }
+    }
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filtraIniziali(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
