88fe1d11f3c563f1429104830f29345a95c750ca
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-09 18:13:15 +0100

    feat esercizio cerca vocali nella parola

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(parola){
+    let vocali = 0;
+    for (let i = 0; i < parola.length; i++){
+        if (parola[i] === 'a' || parola[i] === 'e' || parola[i] === 'i' || parola[i] === 'o' || parola[i] === 'u'){
+            vocali++;
+        }
+    }
+    return vocali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(contaVocali(word));
+
 
 
 
