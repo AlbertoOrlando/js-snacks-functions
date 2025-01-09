f199530d603086419aee83ef2616b04c9f434350
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-09 17:55:54 +0100

    feat esercizio prime lettere

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,9 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function primeLettere(nuovoArray){
+    let iniziali = [];
+    for (let i = 0; i < nuovoArray.length; i++){
+        iniziali.push(nuovoArray[i][0]);
+    }
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(primeLettere(names));
+
 
 
 
